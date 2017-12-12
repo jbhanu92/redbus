2ca08ee41140a301125fc3fb3f99217961142223
1 <1@gmail.cpm>
2017-12-12 14:03:15 +0530

    adding

---
+++ src/main/java/com/bus/App.java
@@ -0,0 +1,27 @@
+package com.bus;
+
+/**
+ * Hello world!
+ *
+ *
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+} */
+
+public class App{
+
+    public static void main(String[] args){
+        
+         for(int i=1;i<=10;i++){
+
+             System.out.println("Hello, World... "+i);
+       }
+
+    }
+
+}
+
